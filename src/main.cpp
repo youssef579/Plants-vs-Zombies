757c8855253af1afaa9899f25b2526909f727325
Youssef Ragaey <youssefragaa3@gmail.com>
2026-03-13 18:00:03 +0200

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Window/WindowEnums.hpp>
+
+int main() {
+  sf::RenderWindow window(sf::VideoMode({900, 600}), "Plants vs Zombies",
+                          sf::Style::Close | sf::Style::Titlebar);
+  window.setFramerateLimit(60);
+  sf::CircleShape shape(100.f);
+  shape.setFillColor(sf::Color::Green);
+
+  const auto onClose = [&window](const sf::Event::Closed &) { window.close(); };
+
+  while (window.isOpen()) {
+    window.handleEvents(onClose);
+
+    window.clear();
+    window.draw(shape);
+    window.display();
+  }
+}
