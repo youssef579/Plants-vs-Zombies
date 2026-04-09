ac210a4b76d82ca20b07abef05ad8cd4394a70dd
Youssef Ragaey <youssefragaa3@gmail.com>
2026-04-09 09:28:32 +0200

    Group plants spritesheets into `Plants` folder in assets

--- src/Plants/Wallnut.cpp
+++ src/Plants/Wallnut.cpp
@@ -1,17 +1,16 @@
 #include <Plants/SunFlower.hpp>
 #include <SunManager.hpp>
 #include <Plants/Plant.hpp>
-#include <iostream>
 
 Plant createWallnut(float x, float y /* the way used in grid */) {
-  static sf::Texture& wallNutTexture = getTexture("assets/wallnut.png");
+  static sf::Texture& wallNutTexture = getTexture("assets/Plants/wallnut.png");
   sf::Sprite wallNutSprite(wallNutTexture);
   wallNutSprite.setTextureRect({{0, 0}, {65, 73}});
 
   Plant newPlant = {wallNutSprite, 100, PlantType::WALLNUT, 0, nullptr};
   Spritesheet wallNutSheet = Spritesheet{&newPlant.sprite, 65, 73, 16, 0.04f};
   newPlant.sheet = wallNutSheet;
-  
+
   newPlant.sprite.setOrigin(newPlant.sprite.getLocalBounds().size / 2.0f);
 
   newPlant.sprite.setPosition({x, y});
@@ -22,7 +21,7 @@ Plant createWallnut(float x, float y /* the way used in grid */) {
 void updateWallnut(Plant &wallNut, float dt){
   animateSpritesheet(wallNut.sheet, dt);
   if (wallNut.health <= 50 && !wallNut.timer){
-    static sf::Texture& wallNutCrackTexture = getTexture("assets/wallnutCrack.png");
+    static sf::Texture& wallNutCrackTexture = getTexture("assets/Plants/wallnutCrack.png");
     sf::Sprite wallNutCrackSprite(wallNutCrackTexture);
     wallNutCrackSprite.setTextureRect({{0, 0}, {65, 73}});
 
@@ -31,13 +30,13 @@ void updateWallnut(Plant &wallNut, float dt){
     wallNut = {wallNutCrackSprite, 50, PlantType::WALLNUT, 1, nullptr};
     Spritesheet wallNutSheet = Spritesheet{&wallNut.sprite, 65, 73, 11, 0.0581f};
     wallNut.sheet = wallNutSheet;
-  
+
     wallNut.sprite.setOrigin(wallNut.sprite.getLocalBounds().size / 2.0f);
 
     wallNut.sprite.setPosition(wallNutPostion);
 
   }else if (wallNut.health <= 20 && wallNut.timer == 1){
-    static sf::Texture& wallNutCrack2Texture = getTexture("assets/wallnutCrack2.png");
+    static sf::Texture& wallNutCrack2Texture = getTexture("assets/Plants/wallnutCrack2.png");
     sf::Sprite wallNutCrack2Sprite(wallNutCrack2Texture);
     wallNutCrack2Sprite.setTextureRect({{0, 0}, {65, 73}});
 
@@ -46,11 +45,11 @@ void updateWallnut(Plant &wallNut, float dt){
     wallNut = {wallNutCrack2Sprite, 20, PlantType::WALLNUT, 2, nullptr};
     Spritesheet wallNutSheet = Spritesheet{&wallNut.sprite, 65, 73, 15, 0.0426f};
     wallNut.sheet = wallNutSheet;
-  
+
     wallNut.sprite.setOrigin(wallNut.sprite.getLocalBounds().size / 2.0f);
 
     wallNut.sprite.setPosition(wallNutPostion);
-    
+
   }
 }
 
