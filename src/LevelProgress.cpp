8e2dea0f3f4c1a46ae0a1ce67202a09c146f8c85
Youssef Ragaey <youssefragaa3@gmail.com>
2026-05-03 21:01:53 +0300

    Use level manager timer

--- src/LevelProgress.cpp
+++ src/LevelProgress.cpp
@@ -1,22 +1,24 @@
-#include <LevelManager.hpp>
 #include <AssetsManager.hpp>
+#include <LevelManager.hpp>
 #include <LevelProgress.hpp>
 #include <cmath>
 
 void drawLevelProgress(float dt) {
-  static sf::Texture &emptyProgressTexture = getTexture("assets/levelProgress/FlagMeter.png");
+  static sf::Texture &emptyProgressTexture =
+      getTexture("assets/levelProgress/FlagMeter.png");
   static sf::Sprite emptyProgressSprite(emptyProgressTexture);
 
-  static sf::Texture &levelProgressTexture = getTexture("assets/levelProgress/FlagMeterLevelProgress.png");
+  static sf::Texture &levelProgressTexture =
+      getTexture("assets/levelProgress/FlagMeterLevelProgress.png");
   static sf::Sprite levelProgressSprite(levelProgressTexture);
 
-  static sf::Texture &progressBarTexture = getTexture("assets/levelProgress/ProgressBar.png");
+  static sf::Texture &progressBarTexture =
+      getTexture("assets/levelProgress/ProgressBar.png");
   static sf::Sprite progressBarSprite(progressBarTexture);
 
-  static sf::Texture &zombieHeadTexture = getTexture("assets/levelProgress/ZombieHead.png");
+  static sf::Texture &zombieHeadTexture =
+      getTexture("assets/levelProgress/ZombieHead.png");
   static sf::Sprite zombieHeadSprite(zombieHeadTexture);
-
-  static float timer = 0;
 
   static bool runOnce = true;
   if (runOnce) {
@@ -25,27 +27,36 @@ void drawLevelProgress(float dt) {
     runOnce = false;
   }
 
-  if (levelManager.currentWave == 0) return;
+  if (levelManager.currentWave == 0)
+    return;
 
-  timer += dt;
-  float offset = progressBarTexture.getSize().x * std::min(
-    timer / (
-      levelManager.levels[levelManager.currentLevel]->waves[levelManager.levels[levelManager.currentLevel]->numberOfWaves - 1]->delay
-      - levelManager.levels[levelManager.currentLevel]->waves[0]->delay
-    ),
-    1.f
-  );
+  float offset =
+      progressBarTexture.getSize().x *
+      std::min(
+          levelManager.timer /
+              (levelManager.levels[levelManager.currentLevel]
+                   ->waves[levelManager.levels[levelManager.currentLevel]
+                               ->numberOfWaves -
+                           1]
+                   ->delay -
+               levelManager.levels[levelManager.currentLevel]->waves[0]->delay),
+          1.f);
 
   progressBarSprite.setPosition({1101 - std::floor(offset), 578.5});
-  progressBarSprite.setTextureRect({{0, 0}, {(int)offset, (int)progressBarTexture.getSize().y}});
+  progressBarSprite.setTextureRect(
+      {{0, 0}, {(int)offset, (int)progressBarTexture.getSize().y}});
 
   zombieHeadSprite.setPosition({1094 - std::floor(offset), 567});
 
   window->draw(emptyProgressSprite);
   window->draw(progressBarSprite);
-  for (int i = 0; i < levelManager.levels[levelManager.currentLevel - 1]->numberOfWaves; i++) {
+  for (int i = 0;
+       i < levelManager.levels[levelManager.currentLevel - 1]->numberOfWaves;
+       i++) {
     if (levelManager.levels[levelManager.currentLevel]->waves[i]->isBigWave)
-      window->draw(levelManager.levels[levelManager.currentLevel - 1]->waves[i]->waveSprite);
+      window->draw(levelManager.levels[levelManager.currentLevel - 1]
+                       ->waves[i]
+                       ->waveSprite);
   }
   window->draw(levelProgressSprite);
   window->draw(zombieHeadSprite);
